7f444d162415d8c4a5f8f9fa2f06f386451bb661
Timothy Roe, Jr <timroejr@msn.com>
2015-06-17 22:54:01 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+This piece of software is a simple use developing ASCII Art. This is a submission for school.
+
+# Plagiarism
+Plagiarism is defined as "the practice of taking someone else's work or ideas and passing them off as one's own." You may not steal this work for your own benefit in a school scenario. You may use this piece of code for reference. This piece of code was developed under a academic honesty policy and is committed to being under that policy.
+
+# Contact
+For questions please contact: troe@roeboatcorp.com
---
+++ src/ArtWork.java
@@ -0,0 +1,27 @@
+
+public class ArtWork {
+
+	public static void main(String[] args) {
+		String row1 = "_________________________________";
+		String row2 = "| <_                             |";
+		String row3 = "| *  * **** *     *      ***     |";
+		String row4 = "| *  * *    *     *     *   *    |";
+		String row5 = "| **** **** *     *     *   *    |";
+		String row6 = "| *  * *    *     *     *   *    |";
+		String row7 = "| *  * **** ***** *****  ***     |";
+		String row8 = "|                                |";
+		String row9 = "----------------------------------";
+		
+		System.out.println(row1);
+		System.out.println(row2);
+		System.out.println(row3);
+		System.out.println(row4);
+		System.out.println(row5);
+		System.out.println(row6);
+		System.out.println(row7);
+		System.out.println(row8);
+		System.out.println(row9);
+
+	}
+
+}
